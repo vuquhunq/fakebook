3813c1b282faa42c94a5bdb59ce759cda803af37
Quang Hung <mo_oc@MacBook-Pro.local>
2022-01-12 20:11:25 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
 import Friends from "./pages/Friends/Friends";
 import Home from "./pages/Home/Home";
 import Profile from "./pages/Profile/Profile";
-
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Login />}/>
-      <Route path='/register' element={<Register />} />
-      <Route path='/home' element={<Home />}/>
-      <Route path='/friends' element={<Friends />}/>
-      <Route path='/profile' element={<Profile />}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/friends" element={<Friends />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </BrowserRouter>
   );
 }
